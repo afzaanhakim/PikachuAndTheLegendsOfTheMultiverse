5343977d15bbd05895ff18e68a0fa3704a5f8b1e
afzaanhakim <afzaanhakim19@gmail.com>
2021-12-26 20:34:08 -0500

    fixed major bug with metadata format to be encoded for os, deployed contract, added metadata for nfts

--- contracts/PokemonNFTGame.sol
+++ contracts/PokemonNFTGame.sol
@@ -2,9 +2,18 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+// NFT contract inheritance
 
-contract PokemonNFTGame {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "hardhat/console.sol";
+// Helper we wrote to encode in Base64
+import "./libraries/Base64.sol";
+
+contract PokemonNFTGame is ERC721 {
   // storing pokemon data in a struct
   //each pokemonattribute holds the base attribute of each chaarcter
   struct PokemonAttributes {
@@ -16,8 +25,17 @@ contract PokemonNFTGame {
     uint256 maxHp;
     uint256 attackDamage;
   }
+  // The tokenId is the NFTs unique identifier, it's just a number that goes
+  // 0, 1, 2, 3, etc.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
   PokemonAttributes[] defaultPokemons; //storing each character in an array
+
+  //creating two mappings (state variables) one for the tokenId => nbftAttributes and the other for the address of the ownerr who owns the nft and reff it later
+  mapping(uint256 => PokemonAttributes) public nftHolderAttributes;
+
+  mapping(address => uint256) public nftHolders;
 
   constructor(
     string[] memory pokemonNames,
@@ -25,7 +43,11 @@ contract PokemonNFTGame {
     uint256[] memory pokemonHp,
     uint256[] memory pokemonAttackDmg,
     string[] memory pokemonTypes
-  ) {
+  )
+    //special indentifiers symbol for NFT
+
+    ERC721("Pikachu and The Legends Of The Multiverse", "MultiverseLegend")
+  {
     for (uint256 i = 0; i < pokemonNames.length; i += 1) {
       defaultPokemons.push(
         PokemonAttributes({
@@ -41,11 +63,84 @@ contract PokemonNFTGame {
 
       PokemonAttributes memory c = defaultPokemons[i];
       console.log(
-        "Done initializing %s w/ type %s, img %s",
+        "Done initializing %s w/ type %s, image %s",
         c.name,
         c.pokemonType,
         c.imageUri
       );
     }
+
+    _tokenIds.increment(); //incrementing token ids
+  }
+
+  function mintPokemonNFT(uint256 _pokemonIndex) external {
+    // Get current tokenId (starts at 1 since we incremented in the constructor).
+    uint256 newItemId = _tokenIds.current();
+
+    _safeMint(msg.sender, newItemId); //assigns the token id to the owners eth address
+
+    //mapping tokenId => character attributes
+    nftHolderAttributes[newItemId] = PokemonAttributes({
+      pokemonIndex: _pokemonIndex,
+      name: defaultPokemons[_pokemonIndex].name,
+      imageUri: defaultPokemons[_pokemonIndex].imageUri,
+      pokemonType: defaultPokemons[_pokemonIndex].pokemonType,
+      hp: defaultPokemons[_pokemonIndex].hp,
+      maxHp: defaultPokemons[_pokemonIndex].maxHp,
+      attackDamage: defaultPokemons[_pokemonIndex].attackDamage
+    });
+
+    console.log(
+      "Minted NFT w/ tokenId %s and characterIndex %s",
+      newItemId,
+      _pokemonIndex
+    );
+
+    // Keep an easy way to see who owns what NFT.
+    nftHolders[msg.sender] = newItemId;
+
+    // Increment the tokenId for the next person that uses it.
+    _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId)
+    public
+    view
+    override
+    returns (string memory)
+  {
+    PokemonAttributes memory pokeAttributes = nftHolderAttributes[_tokenId];
+
+    string memory strHp = Strings.toString(pokeAttributes.hp);
+    string memory strMaxHp = Strings.toString(pokeAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(
+      pokeAttributes.attackDamage
+    );
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "',
+            pokeAttributes.name,
+            " -- NFT #: ",
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Pikachu And The Legends Of The Multiverse!", "image": "',
+            pokeAttributes.imageUri,
+            '", "attributes": [{ "trait_type": "Health Points", "value": ',
+            strHp,
+            ', "max_value":',
+            strMaxHp,
+            '}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,
+            "} ]}"
+          )
+        )
+      )
+    );
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+    return output;
   }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 const main = async () => {
   //compiling our pokemonnftgame contract and generating files with Hardhat / HRE which is used when hardhat script is run
   const gameContractFactory = await hre.ethers.getContractFactory(
-    'PokemonNFTGame'
+    "PokemonNFTGame"
   );
   const gameContract = await gameContractFactory.deploy(
     //names
@@ -30,36 +30,36 @@ const main = async () => {
       "Calyrex",
     ],
     [
-      "https://i.imgur.com/Icd15jl.png", //pikachu
-      "https://imgur.com/dWxTpVV.png", //dialga
-      "https://imgur.com/6FFzCoL.png", //hooh
-      "https://imgur.com/GwWvre2.png", //celebi
-      "https://imgur.com/vGzpDxV.png", //mew
-      "https://imgur.com/T9T7kPk.png", //mewtwo
-      "https://imgur.com/YJrP5SD.png", //rayquaza
-      "https://imgur.com/3Hf5gcZ.png", //zapdos
-      "https://imgur.com/tOEe1JJ.png", //groudon
-      "https://imgur.com/YjFSlsd.png", // giratina
-      "https://imgur.com/lQ89J94.png", //deoxyx
-      "https://imgur.com/uu7Rf9Y.png", //kyurem
-      "https://imgur.com/5YirvMb.png", //virizion
-      "https://imgur.com/u6U1FBh.png", //tornadus
-      "https://imgur.com/GaLVcXO.png", //reshiram
-      "https://imgur.com/SLLp55E.png", //xerneas
-      "https://imgur.com/21eglku.png", //silvally
-      "https://imgur.com/Z6X3Rgj.png", //cosmoem
-      "https://imgur.com/tJBLJS0.png", //zacian
-      "https://imgur.com/4bKuCtq.png", //eternatus
-      "https://imgur.com/RF52Qzc.png", //galarian moltres
-      "https://imgur.com/N2OrjEF.png", //calyrex
+      "https://i.imgur.com/Icd15jl.png",
+      "https://imgur.com/dWxTpVV.png",
+      "https://imgur.com/6FFzCoL.png",
+      "https://imgur.com/GwWvre2.png",
+      "https://imgur.com/vGzpDxV.png",
+      "https://imgur.com/T9T7kPk.png",
+      "https://imgur.com/YJrP5SD.png",
+      "https://imgur.com/3Hf5gcZ.png",
+      "https://imgur.com/tOEe1JJ.png",
+      "https://imgur.com/YjFSlsd.png",
+      "https://imgur.com/lQ89J94.png",
+      "https://imgur.com/uu7Rf9Y.png",
+      "https://imgur.com/5YirvMb.png",
+      "https://imgur.com/u6U1FBh.png",
+      "https://imgur.com/GaLVcXO.png",
+      "https://imgur.com/SLLp55E.png",
+      "https://imgur.com/21eglku.png",
+      "https://imgur.com/Z6X3Rgj.png",
+      "https://imgur.com/tJBLJS0.png",
+      "https://imgur.com/4bKuCtq.png",
+      "https://imgur.com/RF52Qzc.png",
+      "https://imgur.com/N2OrjEF.png",
     ],
     [
       72, 183, 193, 183, 183, 193, 191, 166, 183, 268, 98, 225, 168, 147, 183,
       227, 174, 86, 169, 251, 166, 183,
     ],
     [
-      112, 301, 263, 210, 210, 330, 312, 253, 296, 206, 345, 270, 192, 265,
-      301,275, 198, 54, 279, 305, 251, 162,
+      112, 301, 263, 210, 210, 330, 312, 253, 296, 206, 345, 270, 192, 265, 301,
+      275, 198, 54, 279, 305, 251, 162,
     ],
     [
       "Electric",
@@ -88,17 +88,18 @@ const main = async () => {
   );
   await gameContract.deployed(); //hardhat creating our local network and deploying it
   console.log("Contract deployed to:", gameContract.address);
-   //getting address of our deployed contract on the chain
+  //getting address of our deployed contract on the chain
 
-   let txn;
-   txn = await gameContract.mintPokemonNFT(13)
-   await txn.wait();
-console.log("Minted #14")
+  let txn;
+  txn = await gameContract.mintPokemonNFT(18);
+  await txn.wait();
+  console.log("Minted NFT #19");
 
-   //Getting NFT's token URI
-   let returnedTokenURI = await gameContract.tokenURI(1);
+  let returnedTokenURI = await gameContract.tokenURI(1);
 
-   console.log("Token URI:", returnedTokenURI);
+  console.log("Token URI:", returnedTokenURI);
+
+  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
